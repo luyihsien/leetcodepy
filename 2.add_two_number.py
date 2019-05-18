806c8a1c2ae8983ece87f5a293b74860f09e7126
steven <luyihsien@gmail.com>
2019-05-18 21:16:03 +0800

    'try'

--- 2.add_two_number.py
+++ 2.add_two_number.py
@@ -6,21 +6,25 @@ class ListNode:
 #思路:先把Linked_list倒過來再逐數相加#法二:先放至空List，放完後revese再弄Linkedlist
 class Solution:
     def addTwoNumbers(self, l1, l2):#l1 l2不像List是直接一串，它只保留value以及下一項指向的東東
-        new_head1=None
+        list1=[]
+        list2=[]
         while l1:
-            p=l1
+            list1=list1.append(l1.value)
             l1=l1.next
-            p.next=new_head1
-            new_head1=p
+            list1.reverse()
             #return new_head1
-        new_head2=None
         while l2:#若為空就直接跳出了
-            p=l2#why不直接改l2??#因為改了就沒有人幫跑下一個原本的node
-            l2=l2.next#不是只有值而已而是整顆Node
-            p.next=new_head2#p是已完成反轉的None，暫時存起#不是p=new_head
-            new_head2=p
+            list2=list2.append(l2.value)#why不直接改l2??#因為改了就沒有人幫跑下一個原本的node
+            l2=l2.next
+            list2.reverse()
+        i=0
+        if len(list1)<len(list2):
+            if (len(list2) - len(list1) - 1)>=i:
+                head=ListNode(list2[i])
+                i=i+1
+                head.next=ListNode(list2[1])
+
         #Q:1.Linked list的len是否相同2.倒過來以後0會不會有影響
-        while new_head2
             #return new_head2
 
 
