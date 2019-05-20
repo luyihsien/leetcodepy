7b13345df587689fe18859c678fa3f3ab2db7735
steven <luyihsien@gmail.com>
2019-05-20 14:26:08 +0800

    'try'

--- 27..py
+++ 27..py
@@ -1,4 +1,9 @@
 #error
+b=6
+for i in range(b):#寫下去時b就已經定了，不會跟著改
+    print(b-2)
+    b=b-2
+#4 2 0 -2 -4 -6
 class Solution:
     def removeElement(self, nums, val):
         for i in range(len(nums)):
@@ -6,13 +11,14 @@ class Solution:
                 print('座標'+str(i))
                 print(nums[i])
                 nums.remove(nums[i])
+                i=i-1
                 print(nums)
             else:
                 print('座標'+str(i))
                 print(nums[i])
                 print(nums)
         return len(nums)
-print(Solution().removeElement([0,1,2,2,3,0,4,2],2))
+print(Solution().removeElement([0,1,2,2,3,0,4,2],2))#無論刪的是不是自己，接往下一個走了
 '''
 0
 [0, 1, 2, 2, 3, 0, 4, 2]
