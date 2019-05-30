b002079992edad46b663e57f986450929f4c33ed
steven <luyihsien@gmail.com>
2019-05-30 11:07:18 +0800

    'try'

--- day_test/0530 53 67 121.py
+++ day_test/0530 53 67 121.py
@@ -2,16 +2,17 @@ class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        low=0
+        #low=0#不可設0 因為其差其實不是賣-買的結果
         #m=0
-        m=float('-inf')
+        #m=float('-inf')#應設為0不然負值全進去了
         for i in range(len(prices)):
-            print(i)
+            print(len(prices))
+            print(i)#0 1 2 3 4 5
             #if prices[i]>low:#最小的給low
             if prices[i]<low:
                 low=prices[i]
                 print('low',low)
-            if m<prices[i]-low:
+            if m<prices[i]-low:#m至少要在for此列以上or def與for之間放入，不可直接訂一個沒用過的m
                 m=prices[i]-low
         return m
 print(Solution().maxProfit([7,1,5,3,6,4]))
