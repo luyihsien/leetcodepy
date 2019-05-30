55f420d208f75c75a9a83cdf8cf0921ba530a92e
steven <luyihsien@gmail.com>
2019-05-30 11:44:54 +0800

    'try'

--- day_test/0530 53 67 121.py
+++ day_test/0530 53 67 121.py
@@ -2,12 +2,15 @@ class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        #low=0#不可設0 因為其差其實不是賣-買的結果
+        #low=0#不可設0 因為其差其實不是賣-買的結果也並不是用low表示其差，而是最小買入點，若設為0，即什麼買入成本都不用，若數值全正數，跑出來的結果不可能正確
         #m=0
         #m=float('-inf')#應設為0不然負值全進去了
+        low=prices[0]#low=float('inf')??
+        low=float('inf')
+        m=0#最糟是0
         for i in range(len(prices)):
-            print(len(prices))
-            print(i)#0 1 2 3 4 5
+            print('len(price)',len(prices))
+            print('i第幾項',i)#0 1 2 3 4 5
             #if prices[i]>low:#最小的給low
             if prices[i]<low:
                 low=prices[i]
--- day_test/88 100 101 0530.py
+++ day_test/88 100 101 0530.py
@@ -29,12 +29,31 @@ class Solution:
     def isSameTree(self, p, q):
         if not p and not q:
             return True
-        if not p or not q:
+        elif not p or not q:
             return False
         if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSametree(p.right, q.right)
+'''
+正確解法如下
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif p is not None and q is not None:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            else:
+                return False
+        else:
+            return False
 
+'''
 class Solution:
     def isSameTree(self, p,q) :
         def check(p,q):
