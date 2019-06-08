ef28576977aa90f0fc6a0ff13d7b954c68938f60
steven <luyihsien@gmail.com>
2019-06-08 18:47:44 +0800

    codilty solve Q1

--- codility/1.binary gap.py
+++ codility/1.binary gap.py
@@ -1,14 +1,30 @@
 def solution(N):
-    # write your code in Python 3.6
-    c = ''
-    while N > 0:
-        a, b = N // 2, N % 2
-        print('a',a,'b',b)
-        c += str(b)
-        N = a
+    c=''
+    d={}
+    e=[]
+    while N>0:
+        a,b=N//2,N%2
+        c+=str(b)
+        N=a
     c=c[::-1]
     print(c)
-#solution(6)與solution(3)值都11
+    for i in range(len(c)):
+        if c[i]=='1':
+            d[i]=1
+    if not d:
+        return 0
+    else:
+        g=[]
+        for i in d.keys():
+            e.append(i)
+        print(e)
+        for j in range(1,len(e)):
+            f=e[j]-e[j-1]
+            g.append(f)
+        if not g:
+            return 0
+        return max(g)-1
+print(solution(15))
 '''
 a 1 b 1
 a 0 b 1
