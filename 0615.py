954e31d3fe57674b78a3787139e2bd5b7a247b44
steven <luyihsien@gmail.com>
2019-06-16 07:00:58 +0800

    0615Q2

--- 0615.py
+++ 0615.py
@@ -4,14 +4,18 @@ print(a)
 count=0
 b=a[0][0]
 c=0
-for i in range(len(a)):
-    if a[i][0]==b and count<=5:
-        a[c][1]+=a[i][0]
-        count=count+1
-    else:
-        count=1
-        c=c+1
-
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        scores = collections.defaultdict(list)
+        for sid, score in items:
+            scores[sid].append(score)
+        ans = []
+        i = 1
+        for sid in sorted(scores):
+            lst = scores[sid]
+            ans.append([i, sum(heapq.nlargest(5, lst)) // 5])
+            i += 1
+        return ans
 
 
 
