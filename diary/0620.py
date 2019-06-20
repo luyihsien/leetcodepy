3027387301970ac70b43ba8fbb70ea4ffcb06267
steven <luyihsien@gmail.com>
2019-06-20 10:47:33 +0800

    protect init but not protect change varible in class

--- diary/0620.py
+++ diary/0620.py
@@ -8,3 +8,7 @@ class Rectangle:
         return 2*(self.length+self.width)
     def area(self):
         return self.length*self.width
+rec=Rectangle(4,2)
+print(rec.area())
+rec.width=-4
+print(rec.area())#-16#以上指保護了物件始值，未保護物件更動後
