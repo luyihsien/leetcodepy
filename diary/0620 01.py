4ff898b5f6b11a11fd66aa10270277f4ce7338e6
steven <luyihsien@gmail.com>
2019-06-20 12:55:18 +0800

    decorator learn 0602 01.py

---
+++ diary/0620 01.py
@@ -0,0 +1,21 @@
+def test1(x):
+    return x*2
+def test2(x):
+    return x*x*x
+test1(1)
+test2(2)
+def demo(f):#f為一未呼叫函式
+    def f_new(x):#f_new與f 同參數x
+        print(f.__name__)
+        return f(x)
+    return f_new#可看作tmp的function
+f=demo(test1)
+f(2)#test1
+#print(f(2))
+f=demo(test2)
+#print(f(2))
+@demo
+def test3(x):
+    return x*2*x
+a=test3(4)#比起原本的test3多增加了印函數型態名字的功能
+print(a)
