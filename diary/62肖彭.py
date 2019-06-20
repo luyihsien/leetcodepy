e0c50b0660909238105c8f962c8edce094c6b05a
steven <luyihsien@gmail.com>
2019-06-20 19:57:39 +0800

    *args and inside function called which have fixed number argument

--- diary/62肖彭.py
+++ diary/62肖彭.py
@@ -1,9 +1,9 @@
 def demo(f):
-    def f_new(x):
+    def f_new(x):#f_new(y):傳的參數沒有
         print(f.__name__)
         return f(x)
     return f_new
-@demo
+@demo#讓test3=demo(test3)
 def test3(x):
     return x*2*x
 print(test3(1))#test3 return 2
@@ -15,11 +15,25 @@ print(test3(1,2))#TypeError: f_new() takes 1 positional argument but 2 were give
 故修正如下
 '''
 def demo(f):#傳入function f
-    def f_new(*args,**kwargs):#傳遞可隨意伸縮參數，並在return呼叫f函數，共用同樣參數
+    def f_new(*args,**kwargs):#傳遞可隨意伸縮參數，並在return呼叫f函數，定義共用同樣參數使用方式but未呼叫
         print(f.__name__)
         return f(*args,**kwargs)
     return f_new
 @demo
 def test3(x,y):
     return (x,y)
-print(test3(1,2))#test3 return (1,2)
+print(test3(1,2))#此刻才呼叫#test3 return (1,2)
+def b(x,y,z):
+    print(x,y,z)
+def a(x,y,z):
+    print(x,y)#可供內部呼叫
+    return b(x,y,z)
+a(1,2,3)
+'''
+1 2
+1 2 3
+'''
+def a(*args):
+    print(args)
+    return b(*args)
+a(1,2,3)
