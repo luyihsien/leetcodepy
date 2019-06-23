669cf2b22e3e37c1dfc11ad1a50c76f005e92d45
steven <luyihsien@gmail.com>
2019-06-23 10:42:56 +0800

    t

--- 46..py
+++ 46..py
@@ -16,26 +16,48 @@ class Solution:
             res.append(path)
             print('res',res)
             return
-        print('nums',nums)
-
-
         for i in nums:
+            print('非座標而是值未判i=', i)
             if i not in path:
-                print('i',i)
+                print('判後i',i)
                 path.append(i)
                 b = path[:]
                 print('b',b)
                 global c
                 c=c+1
-                print(c)
+                print('c=',c)
+                print('nums',nums,'b',b,'res',res)
                 self.dfs(nums, b, res)
                 print('pop前path',path,'c',c)
                 path.pop()
-                print('pop後path',path)
+                print('pop後path',path,'c',c)
+                print('i最後',i)
+                #print('num最後', nums, 'b', b, 'res', res)
 Solution().permute([1,2,3])
 '''
 作者：zong_mingyang
-链接：https: // leetcode - cn.com / problems / two - sum / solution / quan - pai - lie - by - zong_mingyang /
+链接：https: // leetcode - cn.com / problems / permutations/ solution / quan - pai - lie - by - zong_mingyang /
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+class Solution:
+    def permute(self,nums):
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                print(res)
+                return
+            for i in range(len(nums)):
+                print('i',i)
+                print('nums[:i] + nums[i+1:]',nums[:i] + nums[i+1:],'tmp + [nums[i]]',tmp + [nums[i]])
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
+Solution().permute([1,2,3])
+'''
+作者：powcai
+链接：https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-by-powcai-2/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
