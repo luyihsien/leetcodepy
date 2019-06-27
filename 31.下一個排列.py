92bef89ed11d628173a85c22c12cdc4711ac2115
steven <luyihsien@gmail.com>
2019-06-27 17:15:05 +0800

    test 31 right answer

--- 31.下一個排列.py
+++ 31.下一個排列.py
@@ -16,3 +16,18 @@ a[3:].reverse()#TypeError: can only assign an iterable
 print(a)#[1, 2, 3, 4, 5]
 a[3:][::-1]
 print(a)#[1, 2, 3, 4, 5]
+class Solution:#自己的正確答案
+    def nextPermutation(self, nums):
+        n=-1
+        for i in range(len(nums)-1,0,-1):
+            if nums[i]>nums[i-1]:
+                n=i-1
+                break
+        if n==-1:
+            nums.reverse()
+        else:
+            for i in range(len(nums)-1,n,-1):
+                if nums[n]<nums[i]:
+                    nums[n],nums[i]=nums[i],nums[n]
+                    nums[n+1:]=nums[n+1:][::-1]
+                    break
