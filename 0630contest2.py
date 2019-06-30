fb28323a3f2beabe55c15d65ca50fe70695e0fff
steven <luyihsien@gmail.com>
2019-06-30 22:35:16 +0800

    add contest

--- 0630contest2.py
+++ 0630contest2.py
@@ -1,12 +1,20 @@
-l=15
+c=2
+l=14
+while 1:
+    if c>=l:
+        break
+    else:
+        c=c*2
+
 a=[]
-for i in range(1,l+1):
+for i in range(1,c+1):
     a.append(i)
 print(a)
 b=2
 while l>=b-1:
     if l>=b-1 and l<2*(b-1):
         a[2*(b-1)-(l-(b-1)):2*(b-1)+1]=a[b-1:l+1][::-1]
+        a[l:2*(b-1)-(l-(b-1))+1]=[None]*len(a[l:2*(b-1)-(l-(b-1))+1])
 
     else:
         a[b-1:2*(b-1)+1]=a[b-1:2*(b-1)+1][::-1]
---
+++ contest143weeksol.py
@@ -0,0 +1,67 @@
+class Solution(object):
+    def pathInZigZagTree(self, label):
+        """
+        :type label: int
+        :rtype: List[int]
+        """
+        tree=[[1]]
+        d=cur=num=1
+        while cur<label:
+            num*=2
+            d=(d+1)%2
+            new=list(range(cur+1,cur+num+1))
+            cur+=num
+            if d:
+                tree.append(new)
+            else:
+                tree.append(new[::-1])
+        path=[]
+        curid=tree[-1].index(label)
+        for rowid in range(len(tree)-1,-1,-1):
+            path.append(tree[rowid][curid])
+            curid/=2
+        return path[::-1]
+#print(Solution().pathInZigZagTree(14))
+#世界第四名#luxcaelestis
+#https://zhuanlan.zhihu.com/p/28018082
+def transname (num):
+    if num == 0:
+        return 0
+    else:
+        print('bin(num)',bin(num))
+        bits = len (bin (num)) - 2#得最高位數，與幾個0幾個1無關
+        print('bits',bits)
+        if bits % 2 == 1:
+            print('num',num)
+            return num
+        else:#bits%2==0
+            #print('num ^ ((1 << (bits - 1)) - 1)',num ^ ((1 << (bits - 1)) - 1))
+            #return num ^ ((1 << (bits - 1)) - 1)
+            print('(1 << (bits))-num-1',(1 << (bits))-num-1)
+            return (1 << (bits))-num-1
+class Solution:
+    def pathInZigZagTree(self, label):
+        realname = transname (label)
+        ans = []
+        while realname > 0:
+            ans.append (realname)
+            print('ans append後',ans)
+            #realname >>= 1
+            realname//=2
+            print('realname',realname)
+        ans = ans[::-1]
+        print('ans',ans)
+        ans = [transname (x) for x in ans]
+        print('ans',ans)
+        return ans
+print(Solution().pathInZigZagTree(14))
+#ans[1,2,4,9]為正常排列
+#print(2<<4)
+#print(0b11==3)
+#print(bin(10),len(bin(10)))
+#http://www.runoob.com/python/python-func-bin.html
+print(1<<(2-1)-1)
+print(6^9)
+print(6&9)
+print(~1)
+print(3>>1)#1
