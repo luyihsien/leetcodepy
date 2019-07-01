cbf29a6af3cb9cf0ed4d1a079203bdba6f978384
steven <luyihsien@gmail.com>
2019-07-02 06:12:30 +0800

    hakerrank day22

--- hackerrank/30days code/day22 BSTs.py
+++ hackerrank/30days code/day22 BSTs.py
@@ -1,9 +1,11 @@
+
 c=0
 class Node:
     global c
     def __init__(self,data):
         self.right=self.left=None
         self.data=data
+'''
 class Solution:
     def insert(self,root,data):
         #global c 加在這無效
@@ -48,3 +50,23 @@ for i in range(T):
     data=int(input())#data此名字在此之前都沒出現過
     root=myTree.insert(root,data)
 
+'''
+class Solution:
+    def insert(self, root, data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<root.data:
+                self.insert(root.left,data)
+            else:
+                self.insert(root.right,data)
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())#data此名字在此之前都沒出現過
+    root=myTree.insert(root,data)
+#print((myTree.left).value,((myTree.left).left).value)#AttributeError: 'Solution' object has no attribute 'left'
+print(root.data)
+print(root.left.data)#AttributeError: 'NoneType' object has no attribute 'data'
+print((root.left).data,((root.left).left).data)
