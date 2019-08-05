52659294c034826cd9a3f7476d757f3bde452661
steven <luyihsien@gmail.com>
2019-08-05 09:32:44 +0800

    'try'

--- contest_leetcode/127/127-1.py
+++ contest_leetcode/127/127-1.py
@@ -6,12 +6,20 @@ class Solution:
         for i in range(len(A)):
             d[A[i]]=i
         print(d)
-        while k:
+        while K:
             if b[0]>=0:
+                print(b[0])
+                print(d[b[0]])
+                print(A[d[b[0]]])
+                K=K-1
+                '''
                 A[d[b[0]]=-A[d[b[0]]]
                 k=k-1
             for i in b:
-                if b<0:
+                if i<0:
                     A[d[i]]=-A[d[i]]
                     k=k-1
         return sum(A)
+        '''
+
+Solution().largestSumAfterKNegations([4,2,3],1)
