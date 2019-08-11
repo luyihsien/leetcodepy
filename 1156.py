84501388cc9577edc04920a3f4f488eeaf24183c
steven <luyihsien@gmail.com>
2019-08-11 11:23:22 +0800

    'try'

--- 1156.py
+++ 1156.py
@@ -1,3 +1,4 @@
+import collections
 '''
 import collections
 class Solution:
@@ -16,3 +17,31 @@ class Solution:
                 s.append(s[-1])
                 b-=1
 '''
+class Solution:
+    def maxRepOpt1(self, text: str) -> int:
+        res=0
+        c=collections.Counter(text)
+        n=len(text)
+        r=1
+        for i in range(n):
+            s=i+1
+            print('i',i)
+            while 1:
+                print('res',res)
+                if s<n and text[i]==text[s]:
+                    print('s',s)
+                    s=s+1
+                if s<n and text[i]!=text[s] and c[i]>1:
+                    r=r-1
+                    print('r',r)
+                    if r<0:
+                        break
+                    s=s+1
+                if s<n and text[i]!=text[s] and c[i]<=1:
+                    print('s',s)
+                    break
+            res=max(res,s-r)
+            print('res',res)
+        return res
+#print(Solution().maxRepOpt1(text = "ababa"))
+print(Solution().maxRepOpt1("aaabaaa"))
