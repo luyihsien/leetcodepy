dafc571c40cd04850185b8f0a58eaa0f13ad0280
steven <luyihsien@gmail.com>
2019-08-14 09:22:48 +0800

    'try'

--- leetcode_practice-master/遞迴/46.permutaion.py
+++ leetcode_practice-master/遞迴/46.permutaion.py
@@ -5,8 +5,10 @@ class Solution:#是在計算可能數，不是數量#數量固定就是n個
         else:
             res=[]
             for i,j in enumerate(nums):
-                n=nums[:i]+nums[i+1:]
-                for k in self.permutation(n):
+                n=nums[:i]+nums[i+1:]#n*O(1)=O(n)
+                for k in self.permutation(n):#n*T(n-1)#n源自於for i,j in enumerate(nums):
                     res.append([j]+k)
             return res
 print(Solution().permutation([1,2,3]))
+#https://stackoverflow.com/questions/5363619/complexity-of-recursive-string-permutation-function
+#T(n) = n*T(n-1) + O(n)
