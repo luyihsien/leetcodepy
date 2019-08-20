a21679fb687303dafc57e68d22a2f208e1a79fbb
steven <luyihsien@gmail.com>
2019-08-20 17:10:20 +0800

    'try'

--- 1027.py
+++ 1027.py
@@ -1,3 +1,4 @@
+'''
 import collections
 class Solution:
     def longestArithSeqLength(self, A) -> int:
@@ -8,7 +9,20 @@ class Solution:
         for i in range(n):
             for j in range(i - 1, -1, -1):
                 dp[i][A[i] - A[j]] = max(dp[i][A[i] - A[j]], dp[j][A[i] - A[j]] + 1)
+                #此時dp[i][A[i] - A[j]]=0<誤>以為等於None or會產生error
                 print('dp',dp)
                 ans = max(ans, dp[i][A[i] - A[j]])
         return ans + 1
+        
+'''
+
+
+class Solution:#時複O(n^2)
+    def longestArithSeqLength(self, A):
+        dp = {}
+        for i in range(len(A)):
+            for j in range(i + 1, len(A)):
+                dp[(j, A[j] - A[i])] = dp.get((i, A[j] - A[i]), 1) + 1
+                #print('dp',dp)
+        return max(dp.values())
 print(Solution().longestArithSeqLength([20,1,15,3,10,5,8]))
