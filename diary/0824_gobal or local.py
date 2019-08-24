32eb8293ebc9e844dede6fd78d3d8929973fbe13
steven <luyihsien@gmail.com>
2019-08-24 10:43:07 +0800

    'try'

--- diary/0824_gobal or local.py
+++ diary/0824_gobal or local.py
@@ -1,16 +1,18 @@
 a=[]
-c=4
+#c=4
 def b():
     a.append(4)
     #c+=1
     print(a)
 b()
 print(a)
+
 class A():
     def a(self):
-        c=4
+        c =4
+
         def b():
-            c=c+1
+            c+=1
             print(c)
         b()
 A().a()
